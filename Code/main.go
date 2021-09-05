f62691b9481670c97216ee3c024d8d3fb6bb4920
Souhardya Saha <souhardyasaha98@gmail.com>
2021-09-05 15:15:56 +0600

    Variables

--- Code/main.go
+++ Code/main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
-	fmt.Println(quote.Go())
+
+	// strings
+	var nameOne string = "mario"
+	var nameTwo = "apple"
+	var namethree string
+
+	fmt.Println(
+		nameOne,
+		nameTwo,
+		namethree,
+	)
+
+	nameFour := "newProcessOfStringAssinging"
+	fmt.Println(nameFour)
+
+	// Ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// Bits and memories
+	var range1 int8 = 127
+	var range2 int32 = 255
+
+	fmt.Println(range1, range2)
+
 }
