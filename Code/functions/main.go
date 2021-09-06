bcdea6e445141335fcb70290b35b2531f253b439
Souhardya Saha <souhardyasaha98@gmail.com>
2021-09-06 14:51:55 +0600

    multiple return types in functions

--- Code/functions/main.go
+++ Code/functions/main.go
@@ -23,11 +23,20 @@ func circleArea(r float64) float64 {
 	return math.Pi * r * r
 }
 
+func circleAreaAndCircumference(r float64) (float64, float64) {
+	return math.Pi * r * r, 2 * math.Pi * r
+}
+
 func main() {
-	names := []string{"sa", "asdf123"}
+	// names := []string{"sa", "asdf123"}
 
-	cycleNames(names, sayGreetings)
-	cycleNames(names, sayBye)
+	// cycleNames(names, sayGreetings)
+	// cycleNames(names, sayBye)
 
-	fmt.Printf("The area of circle is %0.3f", circleArea(10.5))
+	// fmt.Printf("The area of circle is %0.3f", circleArea(10.5))
+
+	// return multiple values in a function
+	area, circumference := circleAreaAndCircumference(10.5)
+	fmt.Printf("The area of circle is %0.3f and Circumference is %0.3f", area, circumference)
+
 }
