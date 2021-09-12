99f94700b45b6da581920a76d5e8c01ec782513f
Souhardya Saha <souhardyasaha98@gmail.com>
2021-09-13 01:37:48 +0600

    Learning RabbitMQ

---
+++ rabbitMQ/learning.md
@@ -0,0 +1,3 @@
+AMQP => Advance Message Queue Protocol
+https://www.rabbitmq.com/tutorials/amqp-concepts.html
+![AMQP MODEL](amqp_model.png)
---
+++ rabbitMQ/sender/send.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	amqp "github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable => exchanges survive broker restart
+		false,   // delete when unused
+		false,   // exclusive => only one consumer
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	fmt.Println(q)
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
+}
